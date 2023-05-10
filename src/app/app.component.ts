b07a1d8aa59809a88370c6c2aee1ff4f51b3fdb2
Xanh van Nguyen <xnguyen@dataleaf.nl>
2023-05-10 14:59:21 +0200

    Angular Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HubConnection } from '@microsoft/signalr';
+import * as signalR from '@microsoft/signalr';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularapp';
+
+  private hubConnection!: HubConnection;
+  message = '';
+  messages: string[] = [];
+ 
+  constructor(
+  ) {}
+ 
+  sendMessage(): void {
+    const data = `Sent: ${this.message}`;
+    if (this.hubConnection) {
+      this.hubConnection.invoke('Send', data);
+    }
+    this.messages.push(data);
+  }
+
+  ngOnInit(): void
+  {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+    .withUrl("http://localhost:5108/chatHub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+    this.hubConnection.start().catch((err) => console.error(err.toString()));
+
+    this.hubConnection.on('Send', (data: any) => {
+      const received = `Received: ${data}`;
+      this.messages.push(received);
+    });
+  }
+
 }
